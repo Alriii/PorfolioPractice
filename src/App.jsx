378f64c2e9bfad107f1429ca545e709cee93cdef
Kenneth Imperial <kennethimperial2005@gmail.com>
2026-07-31 23:19:23 +0800

    practice react portfolio

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>👋 Welcome to My First React App</h1>
+
+      <h2>Hi! My name is Ken.</h2>
+
+      <p>
+        This is my first React application. I am currently learning React and
+        building projects to improve my front-end development skills.
+      </p>
+
+      <h3>About Me</h3>
+
+      <ul>
+        <li>🎓 Course: Bachelor of Science in Information Technology</li>
+        <li>💻 Currently Learning: React</li>
+        <li>🚀 Goal: Pass this course and become a skilled developer.</li>
+      </ul>
+
+      <button onClick={() => alert("Thank you for visiting my first React app!")}>
+        Click Me!
+      </button>
+    </div>
+  );
+}
+
+export default App;
