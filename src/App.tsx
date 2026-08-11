8814a463944c1f3ff947ddb6fcff73afa377fa17
Kenneth Imperial <kennethimperial2005@gmail.com>
2026-08-11 13:47:55 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import "./App.css";
-import profile from "./assets/Valid Photo.png";
+import './App.css'
+import profile from './assets/Valid Photo.png'
 
 function App() {
   return (
     <div className="App">
-      
       <h1>👋 Welcome to My First React App</h1>
       <img src={profile} alt="Ken" className="profile" />
       <h2>Hi! My name is Ken.</h2>
@@ -22,11 +21,11 @@ function App() {
         <li>🚀 Goal: Pass this course and become a skilled developer.</li>
       </ul>
 
-      <button onClick={() => alert("Thank you for visiting my first React app!")}>
+      <button onClick={() => alert('Thank you for visiting my first React app!')}>
         Click Me!
       </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
